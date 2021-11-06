d877b908108b8c0061e434e9f935637ef1c03f89
perial dupont <dperial44@gmail.com>
2021-11-06 10:10:14 +0100

    Initial code for basic calculation with add, substract, multiply and divide

---
+++ bin/console_full_project.dart
@@ -0,0 +1,8 @@
+import 'package:console_full_project/src/calculate.dart';
+
+void main() {
+  print('Add: ' + calculateAdd().toString());
+  print('Substract: ' + calculateSubstract().toString());
+  print('Multiply: ' + calculateMultiply().toString());
+  print('Divide: ' + calculateDivide().toString());
+}
---
+++ lib/src/calculate.dart
@@ -0,0 +1,17 @@
+import 'package:calculator/calculator.dart';
+
+int calculateAdd() {
+  return add(27, 7);
+}
+
+int calculateSubstract() {
+  return substract(27, 7);
+}
+
+int calculateMultiply() {
+  return multiply(27, 7);
+}
+
+double calculateDivide() {
+  return divide(27, 7);
+}
