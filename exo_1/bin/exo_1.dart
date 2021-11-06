491ade718e2db1b185915d492efdedd6a1e10de8
perial dupont <dperial44@gmail.com>
2021-11-06 11:32:25 +0100

    the exercise topic have been added

--- exo_1/bin/exo_1.dart
+++ exo_1/bin/exo_1.dart
@@ -1,5 +1,11 @@
 import 'dart:io';
 
+/*
+ * Exercise 1:
+ * Create a program that asks the user to enter their name and their age. 
+ * Print out a message that tells how many years they have to be 100 
+ * years old.
+ */
 void main() {
   stdout.write("What is your name my friend ?");
   String? name = stdin.readLineSync();
