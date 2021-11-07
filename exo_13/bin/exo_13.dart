a2f2219754b60fe0841fa814731b5af508e0274b
perial dupont <dperial44@gmail.com>
2021-11-07 13:36:36 +0100

    Exercise 13 done

--- exo_13/bin/exo_13.dart
+++ exo_13/bin/exo_13.dart
@@ -1,3 +1,13 @@
+import 'dart:math';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  final random = Random();
+  List<int> randList = List.generate(10, (_) => random.nextInt(100));
+
+  print("Initial list is $randList\n");
+  print("Cleaned list is ${printList(randList)} \n");
+}
+
+List<int> printList(List<int> n) {
+  return n.toSet().toList();
 }
