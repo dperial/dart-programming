7ad7ec84b9c94473b9b724cab6e24f61c2453acb
perial dupont <dperial44@gmail.com>
2021-11-07 13:22:19 +0100

    exercise 12 done, with difficulty (list of Fibonacci)

--- exo_12/bin/exo_12.dart
+++ exo_12/bin/exo_12.dart
@@ -1,3 +1,19 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'dart:io';
+
+void main() {
+  stdout.write("How many Fibonacci numbers do you want? ");
+  int chosenNumber = int.parse(stdin.readLineSync()!);
+
+  List<int> result = fibonacciNumbers(chosenNumber);
+  print(result);
+}
+
+// Function to calulcate the Fibonacci numbers
+List<int> fibonacciNumbers(int chosenNumber) {
+  List<int> fibList = [1, 1];
+
+  for (var i = 0; i < chosenNumber; i++) {
+    fibList.add(fibList[i] + fibList[i + 1]);
+  }
+  return fibList;
 }
