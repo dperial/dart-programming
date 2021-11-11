58881e444d4e45ee4acef19349e5274de3646487
perial dupont <dperial44@gmail.com>
2021-11-12 00:34:56 +0100

    Exercise 19 doone!

--- exo_19/bin/exo_19.dart
+++ exo_19/bin/exo_19.dart
@@ -1,3 +1,24 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'dart:io';
+
+void main() {
+  // Empty board
+  List<List<String>> initialBoard =
+      List.generate(3, (_) => List.generate(3, (_) => ' '));
+  drawBoard(initialBoard, 2);
+}
+
+void drawBoard(List<List<String>> board, int currentUser) {
+  /*
+  Takes an initial board and populates it 
+  either with X or with O depending on
+  the currentUser
+  */
+  String move;
+  currentUser == 2 ? move = 'X' : move = 'O';
+
+  stdout.write("Please choose a coordinate: ");
+  List choice = stdin.readLineSync()!.split(" ");
+  print(choice);
+  board[int.parse(choice[0])][int.parse(choice[1])] = move;
+  print(board);
 }
