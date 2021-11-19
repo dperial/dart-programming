5af9e70dda0d09197a001fa1eaabe6825be9d1bc
perial dupont <dperial44@gmail.com>
2021-11-19 19:48:36 +0100

    Add exercise description in exercise 18

--- exo_18/bin/exo_18.dart
+++ exo_18/bin/exo_18.dart
@@ -1,3 +1,23 @@
+/*
+ * As you may have guessed, we are trying to build up to a full tic-tac-toe board.
+ * For now, we will simply focus on checking whether someone has WON the game, 
+ * not worrying about how the moves were made.
+
+ * If a game of Tic Tac Toe is represented as a list of lists, like so:
+
+ * game = [[1, 2, 0],
+ *        [2, 1, 0],
+ *        [2, 1, 1]]
+ * where a 0 means an empty square, a 1 means that player 1 put their token 
+ * in that space, and a 2 means that player 2 put their token in that space.
+
+ * Your task: given a 3 by 3 list of lists that represents a Tic Tac Toe game board, 
+ * tell whether anyone has won, and tell which player won, if any.
+
+ * A Tic Tac Toe win is 3 in a row - either in a row, a column, or a diagonal. 
+ * Don’t worry about the case where TWO people have won - assume that in every board there will only be one winner.
+ */
+
 void main(List<String> args) {
   List<List<int>> gameList = [
     [1, 0, 1],
@@ -27,7 +47,7 @@ void checkWinner(List<List<int>> board) {
   */
 bool rowWinner(List<List<int>> board) {
   // Another way to implement this part
-  /* for (List<int> in board) {
+  /* for (List<int> row in board) {
       if(row.toSet().length == 1) {
         return true;
       } 
